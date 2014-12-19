c19907b3f7702e435b48ead4555025b19dade64a
rfedorov <rfedorov@renderedsource.com>
2014-12-19 16:10:16 +0600

    creating service calls

--- app/service/views.py
+++ app/service/views.py
@@ -6,3 +6,12 @@ from flask import render_template
 @service.route('/index')
 def index():
     return render_template("index.html")
+
+@service.route('/order/', methods=['GET'])
+def order_list():
+    return "orders list response"
+
+
+@service.route('/order/<order_id>', methods=['GET'])
+def order(order_id):
+    return "order {0} details".format(order_id)
